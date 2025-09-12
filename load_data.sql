c75856c6e47bc0e0639206e6377d0ad008b33803
goutamcap <goutampanda1997@gmail.com>
2025-09-13 01:42:59 +0530

    Update load_data.sql

--- load_data.sql
+++ load_data.sql
@@ -1,4 +1,4 @@
-LOAD DATA LOCAL INFILE 'C:\Users\Jatin Kumar Singh\Desktop\data.csv'
+LOAD DATA LOCAL INFILE 'C:\Users\Goutam Panda\Desktop\data.csv'
 INTO TABLE Students
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -6,3 +6,4 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (full_name, course_opted, mobile_no, @enrollment_date, enrolled)
 SET enrollment_date = STR_TO_DATE(@enrollment_date, '%d-%m-%Y');
+
