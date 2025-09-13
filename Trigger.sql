45834fc8ebb8810642b59bddb48bfcdc8c8ac96b
goutamcap <goutampanda1997@gmail.com>
2025-09-14 01:10:11 +0530

    Update Trigger.sql

--- Trigger.sql
+++ Trigger.sql
@@ -4,8 +4,11 @@ CREATE TRIGGER after_student_insert
 AFTER INSERT ON Students
 FOR EACH ROW
 BEGIN
-    INSERT INTO Enrollments (student_id, join_date, course_opted)
-    VALUES (NEW.student_id, NEW.enrollment_date, NEW.course_opted);
+    -- Insert into Enrollments only if enrolled = 1
+    IF NEW.enrolled = 1 THEN
+        INSERT INTO Enrollments (student_id, join_date, course_opted)
+        VALUES (NEW.student_id, NEW.enrollment_date, NEW.course_opted);
+    END IF;
 END$$
 
 DELIMITER ;
